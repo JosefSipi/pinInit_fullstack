3dbe192c151c42f0566fb1486fcbc20810e1d942
Joseph <Josephsipiorski@gmail.com>
2021-04-29 22:12:02 -0500

    second commit- Configuration

---
+++ webpack.config.js
@@ -0,0 +1,31 @@
+const path = require('path')
+
+module.exports = {
+
+    entry: './frontend/index.jsx',
+    output: {
+        //path requires absolute path 
+        path: path.resolve(__dirname),
+        filename: 'app/assets/javascripts'
+    },
+    devtool: 'source-map',
+    resolve: {
+        extensions: [".js", ".jsx", "*"]
+    },
+    module: {
+        rules: [
+            {
+                test: /\.jsx?$/,
+                exclude: /(node_modules)/,
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: ['@babel/env', '@babel/react'] //which presets to run
+                    }
+                },
+            }
+        ]
+    }
+
+
+}
