4edd35c915508b23a6ed1fcdcf1d97edd973d75c
Joseph <Josephsipiorski@gmail.com>
2021-05-10 16:09:28 -0500

    completed user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,8 +7,60 @@ class User < ApplicationRecord
 
     attr_reader :password
 
+    after_initialize :ensuer_session_token
+
+    has_many :boards,
+        primary_key: :id,
+        foreign_key: :owner_id,
+        class_name: :boards
     
+    has_many :pins,
+        primary_key: :id,
+        foreign_key: :pinner_id,
+        class_name: :Pin
+    
+    has_many :likes,
+        primary_key: :id,
+        foreign_key: :liker_id,
+        class_name: :Like
+
+    
+    # has_many :comments,
+    #     as: :commentable
+    # polymorphic association
+
+    # has_many :follows,
+    #     as: :followable
+    # polymorphic association
+  
 
 
+    def self.find_by_cridentials(username, password)
+        user = User.find_by(username: username)
+        if user && user.is_password?(password)
+            user
+        else
+            nil
+        end
+    end
+
+    def is_password?(password)
+        BCrypt::Password.new(self.password_digest).is_password?(password)
+    end
+
+    def password=(password)
+        @password = password
+        self.password_digest = BCrypt::Password.create(password)
+    end
+
+    def ensuer_session_token
+        self.session_token ||= SecureRandom.urlsafe_base64
+    end
+
+    def reset_session_token
+        self.session_token = SecureRandom.urlsafe_base64
+        self.save!
+        self.session_token
+    end
 
 end
