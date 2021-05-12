c641b73dbd16e8b22a885bc6d4d84d3a80c15f90
Joseph <Josephsipiorski@gmail.com>
2021-05-12 16:59:11 -0500

    building modal

--- frontend/reducers/root_reducer.js
+++ frontend/reducers/root_reducer.js
@@ -1,9 +1,11 @@
 import { combineReducers } from 'redux';
 import sessionReducer from './session';
-import entitiesReducer from './entities';
+// import entitiesReducer from './entities';
+import uiReducer from './ui_reducer';
 
 
 export default combineReducers({
     // entities: entitiesReducer,
-    session: sessionReducer
+    session: sessionReducer,
+    ui: uiReducer,
 });
