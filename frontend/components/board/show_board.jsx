925aa8db6c7ebc606ff658b07613bfbd04c3de9b
josef <josephsipiorski@gmail.com>
2021-08-30 20:44:32 -0500

    other user profile bug fix

--- frontend/components/board/show_board.jsx
+++ frontend/components/board/show_board.jsx
@@ -263,29 +263,24 @@ class BoardShow extends React.Component {
          <div className="pin-area-on-board-show" >
             <div className="pin_container" id="pin_container">
                 {pins.map(pin => 
-                <Link className="link-pin-on-board-pins" to={`/pin/${pin.id}`} key={pin.id + 'pin-key'}> 
-                    <div onLoad={this.photoLoaded} id={`card-card-card${pin.id}`} className="card-update" style={{gridRowEnd: `span 45` }, {visibility: 'hidden'}} key={pin.id} onMouseEnter={this.onMouseEnterCall} onMouseLeave={this.onMouseLeaveCall} onClick={() => this.showPin(pin.id)}>
+                <Link to={`/pin/${pin.id}`} onLoad={this.photoLoaded} id={`card-card-card${pin.id}`} className="card-update" style={{gridRowEnd: `span 45` }, {visibility: 'hidden'}} key={pin.id} onMouseEnter={this.onMouseEnterCall} onMouseLeave={this.onMouseLeaveCall} onClick={() => this.showPin(pin.id)}>
 
-                        <div className="outside-edit-pin-board-show">
-
-                        </div>
-
-                         <div className="outside-surrounding-pin-image-div">
-                            <img className="pin-photo" src={pin.photoUrl} alt="pin photo" />
-
-                            <div data-div_id={pin.id} className="the-shade-over-pin" id={`the-shade-over-pin${pin.id}`}>
-                                <div style={ pin.websiteURL.length < 3 ? {display: 'none'} : {display: 'flex'}} className="website-url-div-hoverthing" id={pin.websiteURL} onClick={this.openTheLink}> <img className="arr-in-website" src={window.upRightArrowURL} alt="up arrow" /> {`${pin.websiteURL}`.slice(8, 16)+ "...."}</div>
-                                
-                                {/* <div id={pin.id} className="edit-pen-div-show-board" onClick={this.modalFunction}>
-                                    <img src={window.editPenURL} alt="edit pen" />
-                                </div> */}
-                            </div>
-
-                        </div>
-
-                        <div className="card-title-pin">{pin.title}</div>
+                    <div className="outside-edit-pin-board-show">
 
                     </div>
+
+                    <div className="outside-surrounding-pin-image-div">
+                        <img className="pin-photo" src={pin.photoUrl} alt="pin photo" />
+
+                        <div data-div_id={pin.id} className="the-shade-over-pin" id={`the-shade-over-pin${pin.id}`}>
+                            <div style={ pin.websiteURL.length < 3 ? {display: 'none'} : {display: 'flex'}} className="website-url-div-hoverthing" id={pin.websiteURL} onClick={this.openTheLink}> <img className="arr-in-website" src={window.upRightArrowURL} alt="up arrow" /> {`${pin.websiteURL}`.slice(8, 16)+ "...."}</div>
+                            
+                        </div>
+
+                    </div>
+
+                    <div className="card-title-pin">{pin.title}</div>
+
                 </Link>
                 )}
             </div>
