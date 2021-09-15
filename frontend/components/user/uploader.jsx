5641a11fd8e949c9abb222e2e93b72ced7d672b5
josef <josephsipiorski@gmail.com>
2021-09-15 16:31:55 -0500

    functional edit profile pic modal

--- frontend/components/user/uploader.jsx
+++ frontend/components/user/uploader.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-
+import LoadingIcon from '../pin/loading';
 
 
 class Uploader extends React.Component {
@@ -9,25 +9,30 @@ class Uploader extends React.Component {
         debugger
         this.state = {
             profile_pic: null,
-            id: window.currentUser.id
+            id: window.currentUser.id,
+            loading: false
         };
         
         this.handelSubmit = this.handelSubmit.bind(this);
         this.handelFile = this.handelFile.bind(this);
-        this.handelFileUploaded = this.handelFileUploaded.bind(this);
+        // this.handelFileUploaded = this.handelFileUploaded.bind(this);
     }
 
-    handelFileUploaded(e){
-        debugger
-        console.log(e.currentTarget.value)
-    }
+    // handelFileUploaded(e){
+    //     debugger
+    //     console.log(e.currentTarget.value)
+    // }
     
    
-    handelSubmit(e) {
-        e.preventDefault();
+    handelSubmit(theStateStuff) {
+        // e.preventDefault();
+        debugger
+        console.log('hit handelSubmit')
         const formData = new FormData();
-        formData.append('user[profile_pic]', this.state.profile_pic);
-        formData.append('user[id]', this.props.currentUser.id);
+        formData.append('user[profile_pic]', theStateStuff.profile_pic);
+        formData.append('user[id]', theStateStuff.id);
+        // formData.append('user[profile_pic]', this.state.profile_pic);
+        // formData.append('user[id]', this.props.currentUser.id);
 
         this.props.updateUser(formData)
             .then(() => {
@@ -46,8 +51,23 @@ class Uploader extends React.Component {
 
 
     handelFile(e) {
-        this.setState({ profile_pic: e.currentTarget.files[0] });
+        e.preventDefault();
+        this.setState({loading: true})
+        this.setState({ profile_pic: e.currentTarget.files[0] })
 
+
+        console.log(e.currentTarget.files[0])
+        debugger
+        console.log(e.currentTarget.files[0])
+        console.log('hit handelFile')
+        
+        let theStateStuff = {}
+        theStateStuff.profile_pic = e.currentTarget.files[0]
+        theStateStuff.id = this.state.id
+
+
+            
+        this.handelSubmit(theStateStuff)
     }
 
     
@@ -65,7 +85,7 @@ class Uploader extends React.Component {
             <div className="the-outer-box-modal-editModal">
                 <h1 className="change-pic-edit-modal">Change your picture</h1>
 
-                    <form className='form-uploader-pic' onSubmit={this.handelSubmit}>
+                    {this.state.loading ? <LoadingIcon/> : <div className='form-uploader-pic' >
                     <div className="input-box-modal-file-div">
                             {/* <label htmlFor="input-profile-pic" id='label-profile-pic-upload'>
                                 <input id='input-profile-pic' name='input-profile-pic' type="file" className="input-box-modal-file" onChange={this.handelFile}/>
@@ -78,12 +98,12 @@ class Uploader extends React.Component {
                                     Choose photo
                                 </div>
 
-                                <input name="input-image-profile-pic-uploader" id="input-image-profile-pic-uploader" type="file" onChange={this.handelFileUploaded} onLoadStart={() => console.log('image started uploading')} onLoadedData={() => console.log('the data uploaded')}/>
+                                <input name="input-image-profile-pic-uploader" id="input-image-profile-pic-uploader" type="file" onChange={this.handelFile}/>
                                 {/* <input name="input-image-profile-pic-uploader" id="input-image-profile-pic-uploader" type="file" onChange={this.handelFile} onLoadStart={() => console.log('image started uploading')} onLoadedData={() => console.log('the data uploaded')}/> */}
 
                             </label>
                         </div>
-                    </form>
+                    </div>}
 
 
                 
